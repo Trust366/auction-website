6c3d2c07824271bb32ee94b75640e38ea2c4c35a
Nkechi Elechi <elechinkechi9@gmail.com>
2025-05-02 20:56:48 +0100

    fixed jwt token

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,17 +1,20 @@
 export const generateToken = (user, message, statusCode, res) => {
-    const token = user.generateJsonWebToken();
-    res
-      .status(statusCode)
-      .cookie("token", token, {
-        expires: new Date(
-          Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true,
-      })
-      .json({
-        success: true,
-        message,
-        user,
-        token,
-      });
-  };
+  const token = user.generateJsonWebToken();
+
+  res
+    .status(statusCode)
+    .cookie("token", token, {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // true in Render
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // critical for cross-origin
+    })
+    .json({
+      success: true,
+      message,
+      user,
+      token,
+    });
+};
